e8858da2285fea46e79816873a2fb4c8b8747f31
ElisaIanda <elisaianda01@gmail.com>
2023-10-03 16:31:06 +0200

    Sistemato errore

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -11,32 +11,32 @@
                 @method('put')
 
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" name="title" value="{{ old('title'), comics->title}}" class="form-control" placeholder="Batman">
+                <input type="text" name="title" value="{{ old('title', $comics->title)}}" class="form-control" placeholder="Batman">
 
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea name="description" class="form-control" placeholder="Descrizione Batman">{{ old('description'), comics->description}}</textarea>
+                <textarea name="description" class="form-control" placeholder="Descrizione Batman">{{ old('description', $comics->description)}}</textarea>
 
                 <label for="thumb" class="form-label">Immagine di Copertina</label>
-                <input type="text" name="thumb" value="{{ old('thumb'), comics->thumb}}" class="form-control"
+                <input type="text" name="thumb" value="{{ old('thumb', $comics->thumb)}}" class="form-control"
                     placeholder="https://mycomics.it/wp-content/uploads/sites/16/2023/08/FUMBDCASC0001.jpg">
 
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="number" name="price" value="{{ old('price'), comics->price}}" class="form-control" placeholder="19.00">
+                <input type="number" name="price" value="{{ old('price', $comics->price)}}" class="form-control" placeholder="19.00">
 
                 <label for="series" class="form-label">Serie</label>
-                <input type="text" name="series" value="{{ old('series'), comics->series}}" class="form-control" placeholder="Nome serie">
+                <input type="text" name="series" value="{{ old('series', $comics->series)}}" class="form-control" placeholder="Nome serie">
 
                 <label for="date" class="form-label">Data</label>
-                <input type="date" name="sale_date" value="{{ old('sale_date'), comics->sale_date}}" class="form-control">
+                <input type="date" name="sale_date" value="{{ old('sale_date', $comics->sale_date)}}" class="form-control">
 
                 <label for="type" class="form-label">Tipo</label>
-                <input type="text" name="type" value="{{ old('type'), comics->type}}" class="form-control" placeholder="Tipo">
+                <input type="text" name="type" value="{{ old('type', $comics->type)}}" class="form-control" placeholder="Tipo">
 
                 <label for="artists" class="form-label">Artisti</label>
-                <input type="text" name="artists" value="{{ old('artists'), implode(",",$comics->artists)}}" class="form-control" placeholder="Artisti">
+                <input type="text" name="artists" value="{{ old('artists', implode(",",$comics->artists))}}" class="form-control" placeholder="Artisti">
 
                 <label for="writers" class="form-label">Scrittori</label>
-                <input type="text" name="writers" value="{{ old('writers'), implode(",", $comics->writers)}}" class="form-control" placeholder="Scrittori">
+                <input type="text" name="writers" value="{{ old('writers', implode(",", $comics->writers))}}" class="form-control" placeholder="Scrittori">
 
 
                 <div class="d-flex justify-content-between">
